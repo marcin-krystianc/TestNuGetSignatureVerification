286ce1680f96efce235c6ca514f67ce3169f504e
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-06-16 14:37:32 +0200

    Initial implementation

---
+++ TestNuGetSignatureVerification/PackageVerifier.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Packaging.Signing;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace TestNuGetSignatureVerification;
+
+public class PackageVerifier
+{
+    public static Task CopyToAsync(Stream stream)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task VerifySignatures(int degreeOfParallelism)
+    {
+        var providers = Repository.Provider.GetCoreV3();
+        var settings = global::NuGet.Configuration.Settings.LoadDefaultSettings(null);
+        var globalPackages = SettingsUtility.GetGlobalPackagesFolder(settings);
+        
+        var sources = PackageSourceProvider.LoadPackageSources(settings);
+        /*
+        foreach (var packageSource in sources)
+        {
+            var sourceRepository = Repository.Factory.GetCoreV3(packageSource.Name);
+            var repositorySignatureResource = await sourceRepository.GetResourceAsync<RepositorySignatureResource>(CancellationToken.None);
+            repositorySignatureResource?.UpdateRepositorySignatureInfo();
+        }
+        */
+        
+        var mainSource = sources
+            .Where(x => x.IsEnabled)
+            .Where(x => x.IsHttp)
+            .FirstOrDefault()
+            .Name;
+
+        var packages = Directory
+            .EnumerateFiles(globalPackages, "*.nupkg", SearchOption.AllDirectories)
+            .ToList();
+
+        Console.WriteLine($"Found {packages.Count} packages in '{globalPackages}" );
+        var clientPolicyContext = ClientPolicyContext.GetClientPolicy(settings, NullLogger.Instance);
+
+        var packagePathResolver = new PackagePathResolver(globalPackages);
+        var packageExtractionContext = new PackageExtractionContext(PackageSaveMode.None, XmlDocFileSaveMode.None, clientPolicyContext, NullLogger.Instance);
+
+        var sw = Stopwatch.StartNew();
+        var tasks = packages.AsParallel().WithDegreeOfParallelism(degreeOfParallelism)
+            .Select(async packagePath =>
+            {
+                using var stream = File.Open(packagePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
+                await PackageExtractor.ExtractPackageAsync(mainSource, (Stream)stream, packagePathResolver, packageExtractionContext, CancellationToken.None);
+            }).ToList();
+
+        await Task.WhenAll(tasks);
+
+        sw.Stop();
+        Console.WriteLine($"Verified {packages.Count} packages in '{sw.Elapsed.TotalSeconds} seconds with degree of parallelism={degreeOfParallelism}" );
+    }
+    
+}
---
+++ TestNuGetSignatureVerification/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Build.Locator;
+using TestNuGetSignatureVerification;
+
+Console.WriteLine("Hello, World!");
+
+var instance = MSBuildLocator.RegisterDefaults();
+Console.WriteLine($"Using MSBuild:{instance.MSBuildPath}");
+
+var verifier = new PackageVerifier();
+var degreeOfParallelism = args.Length >= 1 ? Convert.ToInt32(args[0]) : 1;
+await verifier.VerifySignatures(degreeOfParallelism);
