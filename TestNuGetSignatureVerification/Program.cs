5d164306d788d72669b6f1228ac26dc9a16df721
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-07-12 11:13:24 +0200

    MSBuildLocator

--- TestNuGetSignatureVerification/Program.cs
+++ TestNuGetSignatureVerification/Program.cs
@@ -3,11 +3,11 @@
 using Microsoft.Build.Locator;
 using TestNuGetSignatureVerification;
 
-/*
+
 var instance = MSBuildLocator.RegisterDefaults();
 Console.WriteLine($"Using MSBuild:{instance.MSBuildPath}");
-*/
-MSBuildLocator.RegisterMSBuildPath(new[] { @"d:\dotnet\sdk\7.0.305\"});
+
+//MSBuildLocator.RegisterMSBuildPath(new[] { @"d:\dotnet\sdk\7.0.305\"});
 
 var verifier = new PackageVerifier3();
 var degreeOfParallelism = args.Length >= 1 ? Convert.ToInt32(args[0]) : 1;
